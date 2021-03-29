2ae9739be41cb2ab24bc92690d28a8c12db21144
Massire10 <symassire92@yahoo.fr>
2021-03-29 22:57:03 +0200

    Add Vue Router Config

--- resources/js/components/HomeComponent.vue
+++ resources/js/components/HomeComponent.vue
@@ -3,10 +3,10 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Example Component</div>
+                    <div class="card-header">My App</div>
 
                     <div class="card-body">
-                        I'm an example component.
+                        Bienvenue
                     </div>
                 </div>
             </div>
---
+++ resources/js/components/recipes/RecipeListComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+            <ul class="list-group">
+                <li class="list-group-item">Recipe 1<a href="#" class="href"></a></li>
+                <li class="list-group-item">Recipe 2<a href="#" class="href"></a></li>
+                <li class="list-group-item">Recipe 3<a href="#" class="href"></a></li>
+                <li class="list-group-item">Recipe 4<a href="#" class="href"></a></li>
+            </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

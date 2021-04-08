600b87acf5e91e35058a955730767c3e51f65bf5
Massire10 <symassire92@yahoo.fr>
2021-04-08 22:49:19 +0200

    Top

---
+++ commit.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="lodia.ismaila@gmail.com"
+CORRECT_NAME="Massire10"
+CORRECT_EMAIL="symassire92@yahoo.fr"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
